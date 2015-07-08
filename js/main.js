7bd61fe16376735e4fdfba5a2d4a4e950bb65fdd
Rizky Ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2015-07-08 12:44:24 +0700

    function processData(), errorHandler() and output

--- js/main.js
+++ js/main.js
@@ -17,3 +17,32 @@ function loadHandler(event) {
 	var csv = event.target.result;
 	processData(csv);
 }
+
+function processData(csv) {
+	var all = csv.split(/\r\n|\n/);
+	var lines = [];
+	while (all.length) {
+		lines.push(all.shift().split(','));
+	}
+	console.log(lines);
+	drawoutput(lines);
+}
+
+function errorHandler(evt) {
+	if(evt.target.error.name == "NotReadableError") {
+		alert("Canno't read file !");
+	}
+}
+
+function drawOutput(lines){
+	document.getElementById("result").innerHTML = "";
+	var table = document.createElement("table");
+	for (var i = 0; i < lines.length; i++) {
+		var row = table.insertRow(-1);
+		for (var j = 0; j < lines[i].length; j++) {
+			var firstNameCell = row.insertCell(-1);
+			firstNameCell.appendChild(document.createTextNode(lines[i][j]));
+		}
+	}
+	document.getElementById("result").appendChild(table);
+}
